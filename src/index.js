54a89cc77cb2b629b47fd916c5f9f14eaff62017
Andrey Krainov <metal.awp@gmail.com>
2021-03-13 11:20:00 +0300

    init: start task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+            str = str.replace(bracketsConfig[i][0] + bracketsConfig[i][1], "");
+            i = -1;
+        }
+    }
+    return str == "" ? true : false;
+};
